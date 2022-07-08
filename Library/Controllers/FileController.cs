3362dd0bdb42d4ea22a2b66caf518f6b82566f19
Pawel Swider <pawel.swider@billennium.com>
2022-07-08 15:50:55 +0200

    Dodanie pobierania danych i zapisywania do pliku csv w kontrolerze plików (zrób service do tego)

---
+++ Library/Controllers/FileController.cs
@@ -0,0 +1,43 @@
+﻿using CsvHelper;
+using Library.Entities;
+using Library.Services.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+
+namespace Library.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly MyBooksContext _context;
+
+        public FileController(MyBooksContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetData()
+        {
+            //pobrane dane z bazdy danych i wjebane do listy
+            List<BookDto> downloadedData = new List<BookDto>();
+            downloadedData = await _context.Books.Select(x => new BookDto
+            {
+                Id = x.Id,
+                Author = x.Author,
+                Isbn = x.Isbn
+            }).ToListAsync();
+
+            //ten path jest do zmiany na roznych systemach
+            using (var writer = new StreamWriter("C:\\Users\\pswider\\Downloads\\downloadedDataFile.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(downloadedData);
+            }
+            return Ok(downloadedData);
+        }
+    }
+}
